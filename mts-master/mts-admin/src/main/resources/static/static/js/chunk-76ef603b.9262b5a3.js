cf813c0d31983c602df62c78aafdd12c004d9aa1
csuduan <csuduan@qq.com>
2023-05-23 09:51:07 +0800

    移除mts-admin中static文件

--- mts-master/mts-admin/src/main/resources/static/static/js/chunk-76ef603b.9262b5a3.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76ef603b"],{a33c3:function(n,u,c){"use strict";c.r(u);var e,l,a=c("2877"),o={},p=Object(a["a"])(o,e,l,!1,null,null,null);u["default"]=p.exports}}]);
