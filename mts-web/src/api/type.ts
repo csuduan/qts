e0d4fbca5d4fe5d9d8f6aba60db68c579d3de455
csuduan <csuduan@qq.com>
2023-07-01 09:51:07 +0800

    移除web模块

--- mts-web/src/api/type.ts
+++
@@ -1,5 +0,0 @@
-export interface ResponseType extends Promise<any> {
-  data?: object;
-  code?: number;
-  msg?: string;
-}
--- mts-web/src/components/ReTable/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from "vue";
-import epTableProBar from "./src/bar";
-
-export const EpTableProBar = Object.assign(epTableProBar, {
-  install(app: App) {
-    app.component(epTableProBar.name, epTableProBar);
-  }
-});
